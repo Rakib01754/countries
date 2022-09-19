cc2f688212aacba405135cde205646e2b43129d6
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-09-19 14:11:17 +0600

    search added

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-const getData = async () => {
-    const res = await fetch('https://restcountries.com/v3.1/all')
+const getData = async (searchText) => {
+    const res = await fetch(`https://restcountries.com/v3.1/name/${searchText}`)
     const data = await (res.json())
     displayData(data);
 }
 
 const displayData = (countries) => {
     console.log(countries)
-
     countries.map(country => {
-        const { name, flags, capital, timezones } = country
+        const { name, flags, capital, timezones, population, continents } = country
         const newDiv = document.createElement('div')
         newDiv.classList.add('single-items')
         newDiv.innerHTML = `
             <img src="${flags.png}" alt="">
-            <h2>Name : ${name.common}</h2>
-            <h4>Capital: ${capital?.[0]}</h4>
+            <h2>নাম : ${name.common}</h2>
+            <h3> মহাদেশ : ${continents[0]}</h3>
+            
+            <h4>রাজধানী: ${capital?.[0]}</h4>
+            <h5>জনসংখ্যা: ${population}</h5>
             <h5>Time Zone : ${timezones[0]}</h5>
             `
         document.getElementById('country-container').appendChild(newDiv)
     })
 }
 
-getData();
+const searchOption = () => {
+    document.getElementById('country-container').innerText = "";
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value;
+    searchField.value = "";
+    getData(searchText);
+
+}
+
+getData('b');
